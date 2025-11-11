64283a3344642cdea9d6d6f4da53a9bd247ce8c6
Heri Wibowo <147681086+heriwibowo-dev@users.noreply.github.com>
2025-11-11 13:14:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -57,9 +57,9 @@ async function fetchNews(query) {
     apiKey: API_KEY
   });
 
-  const endpoint = "https://api.allorigins.win/raw?url=" +
+ const endpoint = "https://api.allorigins.win/raw?url=" +
   encodeURIComponent(`${BASE_URL}/everything?${params.toString()}`);
-= `${BASE_URL}/everything?${params.toString()}`;
+
 
   try {
     const res = await fetch(endpoint);
